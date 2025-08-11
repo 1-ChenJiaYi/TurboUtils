0adb56e010c2e35450ba73fbdf733bda2e0fe4b0
chenjiayi949 <chenjiayi949@hellobike.com>
2025-08-11 10:48:20 +0800

    feat: update index.ts

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { TbStack } from "./algorithm/tbStack.js";
+export type { tbStack } from "./algorithm/index.d.ts";
+export { TbQueue } from "./algorithm/tbQueue.js";
+export type { tbQueue } from "./algorithm/index.d.ts";
